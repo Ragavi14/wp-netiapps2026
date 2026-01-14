a8d30ce6d7f707928531745a6dcca05dca7f6099
Ragavi14 <ragavi2002l@gmail.com>
2026-01-14 16:24:27 +0530

    svg upload

--- wp-content/themes/twentytwentyfive/functions.php
+++ wp-content/themes/twentytwentyfive/functions.php
@@ -164,3 +164,8 @@ if ( ! function_exists( 'twentytwentyfive_format_binding' ) ) :
 		}
 	}
 endif;
+function allow_svg_upload($mimes) {
+    $mimes['svg'] = 'image/svg+xml';
+    return $mimes;
+}
+add_filter('upload_mimes', 'allow_svg_upload');
